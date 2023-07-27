49f418b7a2322379a5ee964b1a4290847c253fff
KamogeloThebe <maniakdevelopers@gmail.com>
2023-07-27 08:07:11 +0200

    Routes and Screens
    Added required routes and added stateless widgets for all screens

--- lib/common/routes.dart
+++ lib/common/routes.dart
@@ -1,7 +1,9 @@
 import 'package:weight_tracker_app/common/packages.dart';
 
 var appRoutes = {
-  '/': (context) => HomeScreen(),
+  '/': (context) => HomeScreen(
+        title: 'Weight Tracker App',
+      ),
   '/sign_up_screen': (context) => SignupScreen(),
   '/login_screen': (context) => LoginScreen(),
   '/weight_crud_screen': (context) => WeightCrudScreen(),
